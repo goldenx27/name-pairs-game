cad7595dd4d9de32ada8a08494d22a130cad52ca
goldenx27 <35332403+goldenx27@users.noreply.github.com>
2026-08-15 07:27:39 +0300

    Add interactive speech audio markers

---
+++ migrations/0006_interactive_speech_markers.sql
@@ -0,0 +1,4 @@
+ALTER TABLE speech_items ADD COLUMN name_start REAL;
+ALTER TABLE speech_items ADD COLUMN name_end REAL;
+ALTER TABLE speech_items ADD COLUMN response_start REAL;
+ALTER TABLE speech_items ADD COLUMN response_end REAL;
